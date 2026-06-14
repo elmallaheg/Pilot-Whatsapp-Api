d37a08bbc0adc6c91018f9bbe6411f57e5bd6e7a
elmallaheg <elmallah.eg@gmail.com>
2026-06-14 10:26:10 +0300

    feat: Default locale to Arabic (ar) when DEFAULT_LOCALE env is not set

--- app/controllers/concerns/switch_locale.rb
+++ app/controllers/concerns/switch_locale.rb
@@ -14,7 +14,7 @@ module SwitchLocale
     locale ||= locale_from_custom_domain
 
     # if locale is not set in account, let's use DEFAULT_LOCALE env variable
-    locale ||= ENV.fetch('DEFAULT_LOCALE', nil)
+    locale ||= ENV.fetch('DEFAULT_LOCALE', 'ar')
 
     set_locale(locale, &)
   end
@@ -33,47 +33,3 @@ module SwitchLocale
   # We will use the portal locale in such cases
   def locale_from_custom_domain(&)
     return if params[:locale]
-
-    domain = request.host
-    return if DomainHelper.chatwoot_domain?(domain)
-
-    @portal = Portal.find_by(custom_domain: domain)
-    return unless @portal
-
-    @portal.default_locale
-  end
-
-  def locale_from_user
-    return unless @user
-
-    @user.ui_settings&.dig('locale')
-  end
-
-  def set_locale(locale, &)
-    safe_locale = validate_and_get_locale(locale)
-    # Ensure locale won't bleed into other requests
-    # https://guides.rubyonrails.org/i18n.html#managing-the-locale-across-requests
-    I18n.with_locale(safe_locale, &)
-  end
-
-  def validate_and_get_locale(locale)
-    return I18n.default_locale.to_s if locale.blank?
-
-    available_locales = I18n.available_locales.map(&:to_s)
-    locale_without_variant = locale.split('_')[0]
-
-    if available_locales.include?(locale)
-      locale
-    elsif available_locales.include?(locale_without_variant)
-      locale_without_variant
-    else
-      I18n.default_locale.to_s
-    end
-  end
-
-  def locale_from_account(account)
-    return unless account
-
-    account.locale
-  end
-end
